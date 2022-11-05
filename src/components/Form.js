bed6afcb842690c975528408136d75e0332ca7bb
Thomas <thomasrocks128@me.com>
2022-11-05 19:07:50 -0400

    First mockup of To-do list frontend
    Change-Id: I4396e0b02f9de40cc5b70598ba8a901725cf4040

---
+++ src/components/Form.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+
+const Form = () => {
+    const [inputValue, setInputValue] = useState("")
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value)
+    }
+    
+    return (
+        <form className="ui form">
+            <div className="ui grid center aligned">
+                <div className="row">
+                    <div className="column five wide">
+                        <input 
+                            value={inputValue}
+                            onChange={handleInputChange}
+                            type="text" 
+                            placeholder="Enter something to do..." 
+                        />
+                    </div>
+
+                    <div className="column one wide">
+                        <button type="submit" className="ui button circular icon green"><i className="white plus icon"></i></button>
+                    </div>
+                        
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default Form
---
+++ src/components/Todo.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react"
+
+const Todo = ({title}) => {
+    const [isEditing, setIsEditing] = useState(false)
+    const [value, setValue] = useState(title)
+    const [tempValue, setTempValue] = useState(title)
+    const [completed, setCompleted] = useState(false)
+
+    const handleDivDoubleClick = () => {
+    setIsEditing(true)
+    }
+
+    const handleInputKeyDown = (e) => {
+        const key = e.keyCode
+
+        if (key === 13){
+            setValue(tempValue)
+            setIsEditing(false)
+        } else if (key === 27) {
+            setTempValue(value)
+            setIsEditing(false)
+        }
+    }
+
+    const handleInputOnChange = (e) => {
+        setTempValue(e.target.value)
+    }
+
+    const handleButtonClick = () => {
+        setCompleted(true)
+    }
+
+    return (
+        <div className="row" onDoubleClick={handleDivDoubleClick}>
+            {
+            isEditing ? 
+                <div className="column seven wide">
+                    <div className="ui input fluid">
+                        <input 
+                            onChange={handleInputOnChange}
+                            onKeyDown={handleInputKeyDown}
+                            autoFocus={true} 
+                            value={tempValue}
+                        />
+                    </div>
+                </div> :
+                <>
+                    <div className="column five wide">
+                        <h2 className={"ui header" + (completed ? " green" : "")}>{value}</h2>
+                    </div>
+
+                    <div className="column one wide">
+                        <button 
+                            className="ui button circular icon green"
+                            onClick={handleButtonClick}
+                        >
+                            <i className="white check icon"></i></button>
+                    </div>
+
+                    <div className="column one wide">
+                        <button className="ui button circular icon red"><i className="white remove icon"></i></button>
+                    </div>
+                </>
+            }
+            </div>
+        
+    )
+}
+
+export default Todo
